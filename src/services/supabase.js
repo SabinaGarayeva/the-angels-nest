a3d2da3dd4232c9424af80f8231ef82af0c8e26f
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-17 21:12:39 +0100

    Setting up and connect back-end with Supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://jnptzztclduxzvomxhzx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpucHR6enRjbGR1eHp2b214aHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMjU3MjgsImV4cCI6MjAzNjgwMTcyOH0.Kq1mVtsQP48IKTCwcL_hOfyuwvglYMPowDis_q_MhfQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
