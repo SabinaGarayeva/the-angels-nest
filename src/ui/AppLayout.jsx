4739a08baa5cd409a492fb7ceefc1de7a23d5afd
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-17 15:46:49 +0100

    Setting up project

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+
+
+
+
+export default function AppLayout() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
+
+    </div>
+  );
+}
