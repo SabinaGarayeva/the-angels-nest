1b9af158a397180693768f221bac9d19237f3079
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-17 17:06:19 +0100

    Building the app layout

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+  grid-row: 1/-1;
+`;
 
 export default function Sidebar() {
-  return (
-    <aside>
-      Siderbar
-    </aside>
-  )
+  return <StyledSidebar>Siderbar</StyledSidebar>;
 }
