a26bda24ae7f9e2a3d809a801fc1f322b08e84e6
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-26 23:03:22 +0100

    Implementing Dark Mode with Css variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
- export const supabaseUrl = "https://jnptzztclduxzvomxhzx.supabase.co";
+export const supabaseUrl = "https://jnptzztclduxzvomxhzx.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpucHR6enRjbGR1eHp2b214aHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMjU3MjgsImV4cCI6MjAzNjgwMTcyOH0.Kq1mVtsQP48IKTCwcL_hOfyuwvglYMPowDis_q_MhfQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
