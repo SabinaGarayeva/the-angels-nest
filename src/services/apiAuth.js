e3ac521bab8a9a8d6da77224368cc1894f7dd5d6
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-26 18:32:24 +0100

    User SIgnup

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,22 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+  // console.log(data);
+  return data;
+}
+
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -25,8 +42,7 @@ export async function getCurrentUser() {
   return data?.user;
 }
 
-
 export async function logout() {
-  const {error} = await supabase.auth.signOut()
-  if(error) throw new Error(error.message)
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
