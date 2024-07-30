a5bcc810015f503e51f866049f04a886e9f5acca
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-30 15:21:14 +0100

    fixing small bugs

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,7 +4,10 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    window.matchMedia("(prefers-color-scheme: dark").matches,
+    "isDarkMode"
+  );
 
   useEffect(function () {
     if (isDarkMode) {
@@ -12,7 +15,7 @@ function DarkModeProvider({ children }) {
       document.documentElement.classList.remove("light-mode");
     } else {
       document.documentElement.classList.add("light-mode");
-      document.documentElement.classList.remove("dark-mode"); 
+      document.documentElement.classList.remove("dark-mode");
     }
   });
 
